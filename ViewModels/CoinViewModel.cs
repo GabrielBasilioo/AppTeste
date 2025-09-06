fff09111b5cf3f57526455257cf645cb4ed102d5
GabrielBasilioo <bnbiel726@gmail.com>
2025-09-06 11:55:08 -0300

    projeto atualizado

---
+++ ViewModels/CoinViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MauiApp1.Models;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class CoinViewModel : ObservableObject 
+    { 
+        public string _ladoEscolhido = string.Empty;
+        public string _imagem = string.Empty;
+        public string _resultado = string.Empty;   
+
+        public void Flip()
+        {
+            Coin coin = new Coin();
+            _resultado = coin.Jogar(_ladoEscolhido);
+
+        }
+    }
+}
